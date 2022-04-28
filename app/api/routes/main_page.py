dbaf7b72e5b00caaaaf669580446cc0fc51ac96e
aptmess <improfeo@yandex.ru>
2022-04-28 14:33:24 +0300

    refactor schemas with modified info in output query

--- app/api/routes/main_page.py
+++ app/api/routes/main_page.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from fastapi_utils.cbv import cbv
 from sqlalchemy.orm import Query, Session
 
+from app.api.routes.log_route import LogRoute
 from app.core.engine import get_session
 from app.core.models import Post, Topic
 from app.core.utils import get_and_check_total_pages, get_size
@@ -11,9 +12,9 @@ from app.schemas.input_schemas import (
     SearchByTextInputModel,
     SearchByThread,
 )
-from app.schemas.output_schemas import PostOutput
+from app.schemas.output_schemas import PostsOutput
 
-router = APIRouter()
+router = APIRouter(route_class=LogRoute)
 
 
 @cbv(router)
@@ -23,7 +24,7 @@ class MainRoutes:
 
     def get_all_posts_pagination(
         self, body: Pagination, posts: Query
-    ) -> list[PostOutput]:
+    ) -> PostsOutput:
         page_size = body.count or self.page_size
         page = body.page
         if page:
@@ -32,15 +33,20 @@ class MainRoutes:
             get_and_check_total_pages(page, total_posts, page_size)
         else:
             posts = posts.limit(page_size)
-        all_posts = [u.__dict__ for u in posts.all()]
-        return all_posts
+        return {
+            'amount': posts.count(),
+            'result': [u.__dict__ for u in posts.all()],
+        }
 
-    @router.post('/search', response_model=list[PostOutput])
+    @router.post('/search', response_model=PostsOutput)
     def search_post_by_page(self, body: SearchByTextInputModel):
         """
         Эндпойнт /search для поиска постов по тексту:
-            на вход в API приходит запрос с текстом и лимитом числа постов в формате {"text": "золото", "count": 1000},
-            на выходе отдаются все посты (по каждому посту отдаются все доступные в БД данные) с учетом лимита (параметр count), содержащие данный текст (параметр text).
+            на вход в API приходит запрос с текстом и
+                лимитом числа постов в формате {"text": "золото", "count": 1000},
+            на выходе отдаются все посты (по каждому посту отдаются
+                все доступные в БД данные) с учетом лимита (параметр count),
+                содержащие данный текст (параметр text).
 
         Сортировка результатов: от самомого нового к самому старому
 
@@ -54,12 +60,14 @@ class MainRoutes:
 
         return self.get_all_posts_pagination(body, posts)
 
-    @router.post('/by_thread', response_model=list[PostOutput])
+    @router.post('/by_thread', response_model=PostsOutput)
     def search_post_by_thread(self, body: SearchByThread):
         """
         Эндпойнт /by_thread для вывода постов темы:
-            на вход подается id темы и лимит числа сообщений в формате {"thread_id": 114198, "count": 1000},
-            на выходе отдаются посты данной темы (по каждому посту отдаются все доступные в БД данные) с учетом лимита.
+            на вход подается id темы и лимит числа сообщений
+                в формате {"thread_id": 114198, "count": 1000},
+            на выходе отдаются посты данной темы (по каждому посту
+                отдаются все доступные в БД данные) с учетом лимита.
 
         Сортировка результатов: от самомого нового к самому старому
 
@@ -73,12 +81,13 @@ class MainRoutes:
         )
         return self.get_all_posts_pagination(body, posts)
 
-    @router.post('/replies', response_model=list[PostOutput])
+    @router.post('/replies', response_model=PostsOutput)
     def search_post_by_replies(self, body: SearchByReplies):
         """
         Эндпойнт /replies для поиска реплаев:
             на входе подается id поста (пример: {"post_id": 20202744})
-            на выходе отдается список id постов, которые отвечали на это сообщение.
+            на выходе отдается список id постов, которые отвечали на это
+                сообщение.
 
         Сортировка результатов: по возрастанию id
 
